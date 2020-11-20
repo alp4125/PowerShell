a802983d17b4ac8ada1648b4470521ca6ee232d9
Fredrik Wall <fredrik@poweradmin.se>
2020-11-20 01:37:25 +0100

    Add files via upload

---
+++ Windows/Get-Windows10BuildInformation.ps1
@@ -0,0 +1,98 @@
+function Get-Windows10BuildInformation {
+    <#
+        .SYNOPSIS
+            Will get information about the OS Build
+
+        .DESCRIPTION
+            Will get information about the OS Build from a json file.
+            It will provide version, availability date (release date), servicing option and Kb article.
+            The json file is compiled by the author of this function, but information comes from
+            https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html
+
+        .PARAMETER OSBuildVersion
+            Specifies OS Build number.
+            Can handle both 19041.630 and 10.0.19041.630 format.
+
+        .PARAMETER Online
+            Will open default browser with the Url to the KB article
+    
+        .PARAMETER Force
+            If Force parameter are provided the file will be downloaded even if the file is already downloaded.
+    
+        .EXAMPLE
+            C:\PS> Get-Windows10BuildInformation
+            Will show information about the local computers OS build version.
+
+        .EXAMPLE
+            C:\PS> Get-Windows10BuildInformation -OSBuildVersion "10.0.19041.630"
+        
+        .EXAMPLE
+            C:\PS> Get-Windows10BuildInformation -OSBuildVersion "19041.630"
+
+        .EXAMPLE
+            C:\PS> Get-Windows10BuildInformation -OSBuildVersion "19041.630" -Force
+
+        .NOTES
+            NAME:      	Get-Windows10BuildInformation
+            AUTHOR:    	Fredrik Wall, fredrik@poweradmin.se
+            BLOG:		http://www.fredrikwall.se
+            TWITTER:	walle75
+            CREATED:	19/11/2020
+    
+    #>
+    param(
+        [cmdletbinding()]
+        [Parameter(Mandatory = $false)]
+        $OSBuildVersion,
+        [Parameter(Mandatory = $false)]
+        $Url = "https://raw.githubusercontent.com/FredrikWall/PowerShell/master/Windows/Windows10BuildInformation.json",
+        [Parameter(Mandatory = $false)]
+        [switch]$Online,
+        [Parameter(Mandatory = $false)]
+        [switch]$Force
+    )
+    
+    if ($Force) {
+        Invoke-WebRequest -Uri $Url -OutFile "$PSScriptRoot\Windows10BuildInformation.json"
+    }
+    else {
+        if (-not (Test-Path "$PSScriptRoot\Windows10BuildInformation.json")) {
+            Invoke-WebRequest -Uri $Url -OutFile "$PSScriptRoot\Windows10BuildInformation.json"    
+        }
+    }
+
+    $Windows10OSBuildInfo = Get-Content -Raw "$PSScriptRoot\Windows10BuildInformation.json" | ConvertFrom-Json
+
+    if ($null -eq $OSBuildVersion) {
+        $CurrentBuild = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuild
+        $UBR = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").UBR
+        $OSBuildVersion = "$CurrentBuild.$UBR"
+    }
+    else {
+        $OSBuildVersion = $OSBuildVersion
+    }
+
+    if (($OSBuildVersion.split(".")).Count -eq 2) {
+        $OSBuildVersion = $OSBuildVersion
+    }
+
+    if (($OSBuildVersion.split(".")).Count -eq 4) {
+        $OSBuildVersion = "$(($OSBuildVersion.split(".")[2])).$(($OSBuildVersion.split(".")[3]))"
+    }
+
+    if (-not ($Windows10OSBuildInfo | Where-Object { $_.'OS build' -match $OSBuildVersion })) {
+        $OSBuildVersion = $null
+        Return $OSBuildVersion
+    }
+    else {
+        $MyWindows10BuildInfo = $Windows10OSBuildInfo | Where-Object { $_.'OS build' -match $OSBuildVersion }
+
+        if ($Online) {
+            $KB = ($MyWindows10BuildInfo.'Kb article').Replace("KB ","")
+            Start-Process "https://support.microsoft.com/help/$KB"
+        }
+
+        Return $MyWindows10BuildInfo
+
+    }
+}
